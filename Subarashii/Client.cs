4a298a032793fd247c882cc01624983a00db2579
EvgeSidagis <evgenysidagis@gmail.com>
2019-09-13 18:18:15 -0300

    Refactor of Client and Message Decoder.

--- Subarashii/Client.cs
+++ Subarashii/Client.cs
@@ -53,10 +53,7 @@ namespace Subarashii.Core
 
         public string Send(string code, string message)
         {
-            int length = -1;
-            int received = 0;
-            byte[] bytes = new byte[1024];
-
+      
             var request = new MessageBuilder()
                 .PutOperationCode(code)
                 .PutPayload(message)
@@ -64,37 +61,11 @@ namespace Subarashii.Core
 
             Sender.Send(request);
 
-            while (true)
-            {
-                int bytesRec = Sender.Receive(bytes, received, 1024 - received, SocketFlags.None);
-                received += bytesRec;
-
-                if (received >= sizeof(int) && length == -1)
-                {
-                    length = BitConverter.ToInt32(bytes, 0);
-                }
-
-                if (received >= length)
-                {
-                    break;
-                }
-            }
-
-            var response = MessageDecoder.Decode(bytes.Skip(sizeof(int)).Take(length).ToArray());
-            if (!response.IsResponse)
-            {
-                throw new Whaaaa();
-            }
-
-            return MessageDecoder.DecodePayload(response.Payload);
+            return SharedCodeAndDecode();
         }
 
         public string Send<T>(string code, T payload) where T : class
         {
-            int length = -1;
-            int received = 0;
-            byte[] bytes = new byte[1024];
-
             var request = new MessageBuilder()
                 .PutOperationCode(code)
                 .PutPayload(payload)
@@ -102,29 +73,7 @@ namespace Subarashii.Core
 
             Sender.Send(request);
 
-            while (true)
-            {
-                int bytesRec = Sender.Receive(bytes, received, 1024 - received, SocketFlags.None);
-                received += bytesRec;
-
-                if (received >= sizeof(int) && length == -1)
-                {
-                    length = BitConverter.ToInt32(bytes, 0);
-                }
-
-                if (received >= length)
-                {
-                    break;
-                }
-            }
-
-            var response = MessageDecoder.Decode(bytes.Skip(sizeof(int)).Take(length).ToArray());
-            if (!response.IsResponse)
-            {
-                throw new Whaaaa();
-            }
-
-            return MessageDecoder.DecodePayload(response.Payload);
+            return SharedCodeAndDecode();
         }
 
         public void Dispose()
@@ -134,6 +83,37 @@ namespace Subarashii.Core
             Sender.Close();
 
             IsConnected = false;
+        }
+
+        private string SharedCodeAndDecode()
+        {
+            int length = -1;
+            int received = 0;
+            byte[] bytes = new byte[1024];
+
+            while (true)
+            {
+                int bytesRec = Sender.Receive(bytes, received, 1024 - received, SocketFlags.None);
+                received += bytesRec;
+
+                if (received >= sizeof(int) && length == -1)
+                {
+                    length = BitConverter.ToInt32(bytes, 0);
+                }
+
+                if (received >= length)
+                {
+                    break;
+                }
+            }
+
+            var response = MessageDecoder.Decode(bytes.Skip(sizeof(int)).Take(length).ToArray());
+            if (!response.IsResponse)
+            {
+                throw new Whaaaa();
+            }
+
+            return MessageDecoder.DecodePayload(response.Payload);
         }
     }
 }
