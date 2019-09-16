7531f7db619a33d64f370e7ebb21ca03c32972a5
EvgeSidagis <evgenysidagis@gmail.com>
2019-09-16 16:48:35 -0300

    Created a FileRef class for the student files.

---
+++ Model/FileRef.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SubarashiiDemo.Model
+{
+    public class FileRef
+    {
+        public string Path;
+
+        public string Id;
+
+        public string Name;
+    }
+}
