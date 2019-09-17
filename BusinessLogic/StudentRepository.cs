1ce4e7ebb421051d00085ab601835378915f77f9
Evgeny Sidagis <33876416+evgeSidagis@users.noreply.github.com>
2019-09-17 15:00:31 -0300

    Feature/ce ds (#4)
    * Added new Repos to the Solution
    
    * Fixed some errors as requested.
    
    * Well

---
+++ BusinessLogic/StudentRepository.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using SubarashiiDemo.Model;
+
+namespace SubarashiiDemo.BusinessLogic
+{
+    public class StudentRepository : IRepository<Student>
+    {
+        private List<Student> Students;
+        public void Add(Student objectToCreate)
+        {
+            Students.Add(objectToCreate);
+        }
+
+        public void Delete(Student ObjectToDelete)
+        {
+            Students.Remove(ObjectToDelete);
+        }
+
+        public void Modify(Student oldObject, Student newObject)
+        {
+            Dictionary<Subject, int> tempGrades = oldObject.Grades;
+            Dictionary<Subject, List<FileRef>> tempFiles = oldObject.Files;
+            Students.Remove(oldObject);
+            newObject.Files = tempFiles;
+            newObject.Grades = tempGrades;
+            Students.Add(newObject);
+        }
+
+        public List<Student> GetAll()
+        {
+            return Students;
+        }
+    }
+}
---
+++ BusinessLogic/SubjectRepository.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using SubarashiiDemo.Model;
+
+namespace SubarashiiDemo.BusinessLogic
+{
+    public class SubjectRepository : IRepository<Subject>
+    {
+        private List<Subject> Subjects;
+        public void Add(Subject objectToCreate)
+        {
+            Subjects.Add(objectToCreate);
+        }
+
+        public void Delete(Subject ObjectToDelete)
+        {
+            Subjects.Remove(ObjectToDelete);
+        }
+
+        public List<Subject> GetAll()
+        {
+            return Subjects;
+        }
+
+        public void Modify(Subject oldObject, Subject newObject)
+        {
+            Subject toModify = Subjects.Find(v => v.Id == oldObject.Id);
+            toModify.Name = newObject.Name;
+        }
+    }
+}
