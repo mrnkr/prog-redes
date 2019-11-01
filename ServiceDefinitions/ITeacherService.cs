eaee1aea4c91c979b214a97f5afd8c0802cb9688
EvgeSidagis <evgenysidagis@gmail.com>
2019-11-01 18:25:37 -0300

    Implemented TeacherService

---
+++ ServiceDefinitions/ITeacherService.cs
@@ -0,0 +1,17 @@
+﻿using Gestion.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Gestion.Services
+{
+    public interface ITeacherService
+    {
+        void SignupTeacher(Teacher t);
+
+        void MarkUnmarkedMaterial(string teachedId, string studentId, string subjectId,
+             int grade);
+    }
+}
---
+++ Services/TeacherService.cs
@@ -0,0 +1,47 @@
+﻿using Gestion.Model;
+using Gestion.Repository;
+using Gestion.Services;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Gestion.Services
+{
+    public class TeacherService : ITeacherService
+    {
+        private IRepository<Teacher> TeacherRepo { get; }
+        private IRepository<Student> StudentRepo { get; }
+        private IRepository<Subject> SubjectRepo { get; }
+        private ILogger Logger { get; }
+
+        public TeacherService(IRepository<Teacher> teacherRepo, IRepository<Subject> subjectRepo,
+            IRepository<Student> studentRepo, ILogger logger)
+        {
+            TeacherRepo = teacherRepo;
+            SubjectRepo = subjectRepo;
+            StudentRepo = studentRepo;
+            Logger = logger;
+        }
+
+        public void SignupTeacher(Teacher t)
+        {
+            TeacherRepo.Add(t);
+
+            Logger.Log(EventType.TeacherSignup, $"Registered {t.Id} - {t.LastName}, {t.FirstName}");
+        }
+
+        public void MarkUnmarkedMaterial(string teachedId, string studentId, 
+            string subjectId, int grade)
+        {
+            Student student = StudentRepo.Get(studentId);
+            student.AddGrade(subjectId, grade);
+            StudentRepo.Update(student);
+            Subject subject = SubjectRepo.Get(subjectId);
+
+            Logger.Log(EventType.Grading, $"Graded {student.FirstName} , {student.LastName}," +
+                $" in Subject {subject.Name} with grade {grade}");
+        }
+
+       
+    }
+}
